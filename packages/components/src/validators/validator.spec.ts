c7cd319f3aa15f8b547d695e40e7cb4f4b3ff1b3
Sylvia Gonschior <sylvia.gonschior@telekom.de>
2019-12-30 14:31:15 +0100

    add unit tests for validators

--- packages/components/src/validators/validator.spec.ts
+++ packages/components/src/validators/validator.spec.ts
@@ -1,4 +1,35 @@
-import { defaultValidator } from './validator';
+import { defaultValidator, ValidatorEntry } from './validator';
+
+describe('entry validator', () => {
+  it('returns a name', () => {
+    const validatorEntry: ValidatorEntry = {
+      name: 'validator',
+      options: 'z.B. string',
+    };
+    expect(validatorEntry.name).toEqual('validator');
+  });
+  it('returns a string for options', () => {
+    const validatorEntry: ValidatorEntry = {
+      name: 'validator',
+      options: 'z.B. string',
+    };
+    expect(validatorEntry.options).toEqual('z.B. string');
+  });
+  it('returns a number for options', () => {
+    const validatorEntry: ValidatorEntry = {
+      name: 'validator',
+      options: 15,
+    };
+    expect(validatorEntry.options).toEqual(15);
+  });
+  it('returns a boolean for options', () => {
+    const validatorEntry: ValidatorEntry = {
+      name: 'validator',
+      options: false,
+    };
+    expect(validatorEntry.options).toEqual(false);
+  });
+});
 
 describe('defaultValidator', () => {
   it('returns default validator', () => {
