f93a1f8fffdbbad5159449d2fa1179df0d1a7e9b
Daniel Beck <nowseemee@gmail.com>
2020-05-04 22:25:06 +0200

    feat: table with better examples

--- examples/boilerplate-gatsby/src/pages/index.js
+++ examples/boilerplate-gatsby/src/pages/index.js
@@ -4,66 +4,103 @@ import {
   ScaleCard,
   ScaleLink,
   ScaleTag,
-  ScaleTable,
-  ScaleProgressBar
+  ScaleTable
 } from "@scaleds/components-react"
+import { useTable, useSortBy } from "react-table"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h3>Alert</h3>
-    <ScaleLink href="http://example.com" target="_blank" variant="success">
-      Success
-    </ScaleLink>
-    <h3>Button</h3>
-    <ScaleButton variant="primary">Click!</ScaleButton>
-    <h3>Card</h3>
-    <ScaleCard>A title</ScaleCard>
-    <h3>Tag</h3>
-    <ScaleTag dismissable size="small" onClose={event => console.log(event)}>
-      A title
-    </ScaleTag>
+const IndexPage = () => {
+  const columns = React.useMemo(
+    () => [
+      {
+        id: "x01337",
+        Header: "First Name",
+        accessor: "firstName"
+      },
+      {
+        id: "x01338",
+        Header: "Last Name",
+        accessor: "lastName"
+      }
+    ],
+    []
+  )
 
-    <ScaleTable variant="regular">
-      <table>
-        <thead>
-          <tr>
-            <th aria-sort="descending">Title</th>
-            <th>Tags</th>
-            <th>Stats</th>
-            <th style={{ textAlign: "right" }}>Time</th>
-            <th style={{ textAlign: "right" }}>Euros</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>University of Plymouth</td>
-            <td>
-              <ScaleTag style={{ marginRight: "8px" }}>Other</ScaleTag>
-              <ScaleTag style={{ marginRight: "8px" }}>N/A</ScaleTag>
-              <ScaleTag>Demo</ScaleTag>
-            </td>
-            <td>
-              <div style={{ display: "inline-flex", alignItems: "center" }}>
-                <span style={{ marginRight: "8px" }}>1.356</span>
-                <ScaleProgressBar
-                  variant="info"
-                  style={{ maxWidth: "120px" }}
-                  stroke-width="6"
-                  percentage="90"
-                />
-              </div>
-            </td>
-            <td style={{ textAlign: "right" }}>00:00:20</td>
-            <td style={{ textAlign: "right" }}>100.245,10</td>
-          </tr>
-        </tbody>
-      </table>
-    </ScaleTable>
-  </Layout>
-)
+  const data = React.useMemo(() => [{ firstName: "John", lastName: "Doe" }], [])
+
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow
+  } = useTable(
+    {
+      columns,
+      data
+    },
+    useSortBy
+  )
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h3>Alert</h3>
+      <ScaleLink href="http://example.com" target="_blank" variant="success">
+        Success
+      </ScaleLink>
+      <h3>Button</h3>
+      <ScaleButton variant="primary">Click!</ScaleButton>
+      <h3>Card</h3>
+      <ScaleCard>A title</ScaleCard>
+      <h3>Tag</h3>
+      <ScaleTag dismissable size="small" onClose={event => console.log(event)}>
+        A title
+      </ScaleTag>
+
+      <ScaleTable variant="regular">
+        <table {...getTableProps()}>
+          <thead>
+            {headerGroups.map(headerGroup => (
+              <tr {...headerGroup.getHeaderGroupProps()}>
+                {headerGroup.headers.map(column => (
+                  <th
+                    width="200px"
+                    aria-sort={
+                      !column.isSorted
+                        ? "none"
+                        : column.isSortedDesc
+                        ? "descending"
+                        : "ascending"
+                    }
+                    {...column.getHeaderProps(column.getSortByToggleProps())}
+                  >
+                    {column.render("Header")}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody {...getTableBodyProps()}>
+            {rows.map(row => {
+              prepareRow(row)
+              return (
+                <tr {...row.getRowProps()}>
+                  {row.cells.map(cell => {
+                    return (
+                      <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                    )
+                  })}
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+      </ScaleTable>
+    </Layout>
+  )
+}
 
 export default IndexPage
